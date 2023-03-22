3382acd07c63bd8b0d180a247431ae72d24ad972
julia <marek.forgi@proton.me>
2023-03-22 21:24:15 +0100

    Help

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-unit = input("Starting unit F/C ")
+import PySimpleGUI as sg
 
-if unit == "F" or "f":
-    f = float(input("Value: "))
-    c = (f - 32) * 5 / 9
-    print(c)
-elif unit == "C":
-    c = float(input("Value: "))
-    f =  (c * 9 / 5 ) + 32
-    print(f)
-else:    print("what")
+layout = [[sg.Text("F or C ?")],
+          [sg.Input(key='-INPUT-')],
+          [sg.Button('F'), sg.Button('C')],
+          [sg.Button('Ok')],
+          [sg.Text(size=(40,1), key='-OUTPUT-')]]
 
+window = sg.Window("Convert", layout)
+
+
+while True:
+    event, values = window.read()
+    if event in (sg.WINDOW_CLOSED, 'Ok'):
+        break
+    elif  event == 'F':
+        f = float(values['-INPUT-'])
+        c = (f - 32) * 5 / 9
+        window['-OUTPUT-'].update(c)
+    elif event == 'C':
+        c = float(values['-INPUT-'])
+        f =  (c * 9 / 5 ) + 32
+        print(f)
+    else:    print("what")
+
+window.close()
